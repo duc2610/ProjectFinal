ad2b8752ea5f983a09e9a6742dedec5c83f1f7bf
HuyPQ <142916705+pqhuy8723@users.noreply.github.com>
2025-11-01 13:19:51 +0700

    update home Page and fix login

--- frontend/src/shared/layouts/MainLayout.jsx
+++ frontend/src/shared/layouts/MainLayout.jsx
@@ -2,13 +2,15 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import styles from "@shared/styles/MainLayout.module.css";
+
 export default function MainLayout() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className={styles.layout}>
       <Header />
-      <div className={styles.headerUnderbar} />
-      <main className="flex-1">
-        <Outlet />
+      <main className={styles.main}>
+        <div className={styles.content}>
+          <Outlet />
+        </div>
       </main>
       <Footer />
     </div>
