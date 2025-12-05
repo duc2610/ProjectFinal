569fe72e6167e101c83ce4db97b97661d7a00cd1
HP <DucHMHE172440@fpt.edu.vn>
2025-12-05 14:25:35 +0700

    fix grading sw

--- backend/ToeicGenius/Repositories/Interfaces/ITestQuestionRepository.cs
+++ backend/ToeicGenius/Repositories/Interfaces/ITestQuestionRepository.cs
@@ -5,6 +5,7 @@ namespace ToeicGenius.Repositories.Interfaces
 	public interface ITestQuestionRepository : IBaseRepository<TestQuestion, int>
 	{
 		Task<List<TestQuestion>> GetByTestIdAsync(int testId);
+		Task<List<TestQuestion>> GetByTestIdWithPartAsync(int testId);
 		void RemoveRange(IEnumerable<TestQuestion> entities);
 		Task<List<TestQuestion>> GetByTestAndPartAsync(int testId, int partId);
 		Task<TestQuestion?> GetByIdWithDetailsAsync(int testQuestionId);
